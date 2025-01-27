97eb8b8041fb145ca1613fa41a5b411588d230c0
Eray Erer <eray.erer@hotmail.com>
2025-01-27 18:00:26 +0300

    update vocab len

--- containers/rencos-env/rencos-master/onmt/opts.py
+++ containers/rencos-env/rencos-master/onmt/opts.py
@@ -219,11 +219,11 @@ def preprocess_opts(parser):
     group.add('--features_vocabs_prefix', '-features_vocabs_prefix',
               type=str, default='',
               help="Path prefix to existing features vocabularies")
-    group.add('--src_vocab_size', '-src_vocab_size', type=int, default=19026,
+    group.add('--src_vocab_size', '-src_vocab_size', type=int, default=18858,
               help="Size of the source vocabulary")
-    group.add('--ref_vocab_size', '-ref_vocab_size', type=int, default=19026,
+    group.add('--ref_vocab_size', '-ref_vocab_size', type=int, default=18858,
               help="Size of the reference vocabulary")
-    group.add('--tgt_vocab_size', '-tgt_vocab_size', type=int, default=19026,
+    group.add('--tgt_vocab_size', '-tgt_vocab_size', type=int, default=18858,
               help="Size of the target vocabulary")
 
     group.add('--src_words_min_frequency',
