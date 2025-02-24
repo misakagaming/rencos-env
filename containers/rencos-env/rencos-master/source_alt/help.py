97ecf243690b3bd75028ceedf8b74fc334893584
Eray Erer <eray.erer@hotmail.com>
2025-02-24 20:37:35 +0300

    alt data

---
+++ containers/rencos-env/rencos-master/source_alt/help.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('train.csv')
+df1 = df.code
+df1.to_csv('train.src.csv', index=False, header=False)
+df2 = df.summary
+df2.to_csv('train.tgt.csv', index=False, header=False)
+    
+df = pd.read_csv('test.csv')
+df1 = df.code
+df1.to_csv('test.src.csv', index=False, header=False)
+df2 = df.summary
+df2.to_csv('test.tgt.csv', index=False, header=False)
+
+    
+df = pd.read_csv('valid.csv')
+df1 = df.code
+df1.to_csv('valid.src.csv', index=False, header=False)
+df2 = df.summary
+df2.to_csv('valid.tgt.csv', index=False, header=False)
