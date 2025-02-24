b086b3824091351d83c12631a48d192d7c9cb558
Eray Erer <eray.erer@hotmail.com>
2025-02-24 21:05:57 +0300

    help

--- containers/rencos-env/rencos-master/source_alt/help.py
+++ containers/rencos-env/rencos-master/source_alt/help.py
@@ -3,12 +3,18 @@ import numpy as np
 
 df = pd.read_csv('train.csv')
 df1 = df.code
+df1 = df1.replace(r'\n',' ', regex=True)
+df1 = df1.replace(r'\t',' ', regex=True)
+df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('train.src.csv', index=False, header=False)
 df2 = df.summary
 df2.to_csv('train.tgt.csv', index=False, header=False)
     
 df = pd.read_csv('test.csv')
 df1 = df.code
+df1 = df1.replace(r'\n',' ', regex=True)
+df1 = df1.replace(r'\t',' ', regex=True)
+df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('test.src.csv', index=False, header=False)
 df2 = df.summary
 df2.to_csv('test.tgt.csv', index=False, header=False)
@@ -16,6 +22,10 @@ df2.to_csv('test.tgt.csv', index=False, header=False)
     
 df = pd.read_csv('valid.csv')
 df1 = df.code
+df1 = df1.replace(r'\n',' ', regex=True)
+df1 = df1.replace(r'\t',' ', regex=True)
+df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('valid.src.csv', index=False, header=False)
 df2 = df.summary
 df2.to_csv('valid.tgt.csv', index=False, header=False)
+
