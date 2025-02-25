4e60b7679c61cb452dc0ef0793c9e58d2e1944cd
Eray Erer <eray.erer@hotmail.com>
2025-02-25 19:49:01 +0300

    updated commands

--- containers/rencos-env/rencos-master/source_alt/help.py
+++ containers/rencos-env/rencos-master/source_alt/help.py
@@ -8,6 +8,9 @@ df1 = df1.replace(r'\t',' ', regex=True)
 df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('train.src.csv', index=False, header=False)
 df2 = df.summary
+df2 = df2.replace(r'\n',' ', regex=True)
+df2 = df2.replace(r'\t',' ', regex=True)
+df2 = df2.replace(r'\s', ' ', regex = True)
 df2.to_csv('train.tgt.csv', index=False, header=False)
     
 df = pd.read_csv('test.csv')
@@ -17,6 +20,9 @@ df1 = df1.replace(r'\t',' ', regex=True)
 df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('test.src.csv', index=False, header=False)
 df2 = df.summary
+df2 = df2.replace(r'\n',' ', regex=True)
+df2 = df2.replace(r'\t',' ', regex=True)
+df2 = df2.replace(r'\s', ' ', regex = True)
 df2.to_csv('test.tgt.csv', index=False, header=False)
 
     
@@ -27,5 +33,8 @@ df1 = df1.replace(r'\t',' ', regex=True)
 df1 = df1.replace(r'\s', ' ', regex = True)
 df1.to_csv('valid.src.csv', index=False, header=False)
 df2 = df.summary
+df2 = df2.replace(r'\n',' ', regex=True)
+df2 = df2.replace(r'\t',' ', regex=True)
+df2 = df2.replace(r'\s', ' ', regex = True)
 df2.to_csv('valid.tgt.csv', index=False, header=False)
 
